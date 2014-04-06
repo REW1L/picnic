a343ad2de0f0512c40b4b25c1ea85fb7c72787d5
Ivan Danilov <Rew1L@MacBook-Air-Ivan.local>
2014-04-06 19:33:57 +0400

    new file

---
+++ Picnic.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package picnic;
+
+/**
+ *
+ * @author Rew1L
+ */
+public class Picnic {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
